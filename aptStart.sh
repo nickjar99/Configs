1ea812deef0359029decbbd8ecec05f80eb4f93a
nick <nickjar99@gmail.com>
2019-07-05 10:40:52 -0700

    ssh keygen & -y

--- aptStart.sh
+++ aptStart.sh
@@ -1,5 +1,9 @@
+ssh-keygen -f ~/.ssh/id_rsa -N ""
+
 sudo apt-get update
 
-sudo apt-get full-upgrade
+sudo apt-get -y full-upgrade
 
-sudo apt-get install vim nano git neofetch ncdu dialog glances htop wget curl openssh-server zsh
+sudo apt-get install -y vim nano git neofetch ncdu dialog glances htop wget curl openssh-server zsh
+
+sudo apt autoremove -y
