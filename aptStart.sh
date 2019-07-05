99d5cb89fc84b38183d521e9d2921fc7658985a3
nick <nickjar99@gmail.com>
2019-07-05 10:43:47 -0700

    installs oh my zsh; comments added

--- aptStart.sh
+++ aptStart.sh
@@ -1,9 +1,11 @@
 ssh-keygen -f ~/.ssh/id_rsa -N ""
 
+
+# Updating stuff and installing stuff
 sudo apt-get update
-
 sudo apt-get -y full-upgrade
-
-sudo apt-get install -y vim nano git neofetch ncdu dialog glances htop wget curl openssh-server zsh
-
+sudo apt-get install -y vim nano git neofetch ncdu dialog glances htop wget curl openssh-server zsh powerline
 sudo apt autoremove -y
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
