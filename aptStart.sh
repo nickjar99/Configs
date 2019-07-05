5a7ca2817e96e851f179f1ae87092674ec55ce0b
nick <nickjar99@gmail.com>
2019-07-05 10:15:59 -0700

    Added zsh

--- aptStart.sh
+++ aptStart.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 
 sudo apt-get full-upgrade
 
-sudo apt-get install vim nano git neofetch ncdu dialog glances htop wget curl openssh-server
+sudo apt-get install vim nano git neofetch ncdu dialog glances htop wget curl openssh-server zsh
