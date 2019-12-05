f4c5b019eae8bd42cd191b2df50ea114eec81846
Nick <njarmusz@mymail.mines.edu>
2019-12-04 20:21:00 -0700

    moved packages around

--- archVM/archInstallVM2-chroot.sh
+++ archVM/archInstallVM2-chroot.sh
@@ -18,7 +18,7 @@ echo "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t$hostname.localdomain $ho
 # add nick user and passwords
 sed -i "82i%wheel ALL=(ALL) ALL" /etc/sudoers
 useradd -G wheel -m -s /usr/bin/zsh nick
-read -ps "Enter a password for both accounts: " password
+read -sp "Enter a password for both accounts: " password
 echo "$password" | passwd
 echo "$password" | passwd nick
 
@@ -32,7 +32,7 @@ sed -i "131iCOMPRESSBZ2=(pbzip2 -c -f)" /etc/makepkg.conf
 sed -i "133iCOMPRESSZST=(zstd -c -z -q - --threads=0)
 
 # more packages
-pacman -S zstd gcc-libs glances
+pacman -S zstd gcc-libs glances base-devel neofetch ncdu make pigz pbzip2 unzip xz
 
 # Bootloader
 grub-install --target=i386-pc /dev/sda
