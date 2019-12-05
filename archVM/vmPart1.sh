3b7714af5aa79a603b53ad5944473be0cadc7ca0
Nick <njarmusz@mymail.mines.edu>
2019-12-04 23:18:34 -0700

    going back to redownloading

--- archVM/vmPart1.sh
+++ archVM/vmPart1.sh
@@ -20,6 +20,5 @@ cp /root/.zshrc /mnt/root/.zshrc
 cp /root/.zshrc /mnt/root/.zshrc-installer
 genfstab -U /mnt >> /mnt/etc/fstab
 
-cp -r /root/configs /mnt/root/configs
-arch-chroot /mnt /usr/bin/zsh -c "sh /root/config/archVM/vmPart2chroot.sh"
 
+arch-chroot /mnt /usr/bin/zsh -c "cd /root && git clone https://gitlab.com/NickTheSecond/configs.git && cd configs/archVM && sh vmPart2chroot.sh"
