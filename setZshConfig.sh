b7464caa562600dc07e58ac849a05b4e84b69a16
Nick <njarmusz@mymail.mines.edu>
2020-01-07 18:38:52 -0700

    There was an attempt

---
+++ setZshConfig.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REINSTALL=-1
+
+while [ $REINSTALL = -1 ]; do
+    printf "Reinstall oh-my-zsh? (y/n) "
+    read answer
+    if [ ${#answer} != 0 ] && ([ $answer = "y" ] || [ $answer = "Y" ]); then
+        REINSTALL=1
+    elif [ ${#answer} != 0 ] && ([ $answer = "n" ] || [ $answer = "N" ]); then
+        REINSTALL=0
+    else
+        printf "Invalid input, try again.\n"
+    fi
+done
+
+if [ $REINSTALL = 1 ]; then
+    echo "Uninstalling zsh..."
+    rm -rf ~/.oh-my-zsh
+    rm -rf ~/.zshrc.pre-oh-my-zsh
+    mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh
+
+    echo "Reinstalling zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    sed "11 s/robbyrussell/agnoster" ~/.zshrc
+    echo "\nalias ytdl=youtube-dl\n" > ~/.zshrc
+    echo "\nalias gcl=\"git clone\"\n" > ~/.zshrc
+
+else
+    echo "Not reinstalling zsh"
+fi
+
---
+++ wsl/wsl-kali.sh
@@ -0,0 +1,8 @@
+
+
+sudo apt update
+sudo apt upgrade
+sudo apt install openssh-server wget curl git vim nano neofetch htop ncdu unzip rar zsh powerline
+
+
+sudo apt install speedtest-cli hdparm python3-pip glances
