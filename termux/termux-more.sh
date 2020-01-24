1a430d25c0ffa069cad72684a0226ebdcd9006a0
Nick <nickjar99@gmail.com>
2020-01-23 20:24:41 -0700

    faulty package

--- termux/termux-more.sh
+++ termux/termux-more.sh
@@ -1,5 +1,5 @@
 # More packages
-pkg in -y golang rust unzip zip dos2unix unrar tty-clock tigervnc shared-mime-info screenfetch screen ruby ripgrep readline-static pkg-config p7zip ffmpeg opusfile-static opus-tools ninja ncdu imagemagick help2man graphicsmagick golang-doc gdb fdupes feh fakeroot emacs dialog cowsay clang cmake ccache bsdtar bash-completion autoconf automake termux-services termux-apt-repo ccache automake autoconf autossh speedtest-cli
+pkg in -y golang rust unzip zip dos2unix unrar tty-clock tigervnc shared-mime-info screenfetch screen ruby ripgrep readline-static pkg-config p7zip ffmpeg opusfile-static opus-tools ninja ncdu imagemagick help2man graphicsmagick golang-doc gdb fdupes feh fakeroot emacs dialog cowsay clang cmake ccache bsdtar bash-completion autoconf automake termux-services termux-apt-repo ccache automake autoconf autossh
 
 # Python stuff
 pip install --user pylint
