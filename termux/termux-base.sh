871e226db236815abcb1ace141eea25a7fab9d79
Nick <nickjar99@gmail.com>
2020-01-23 19:50:09 -0700

    added youtube-dl, ssh

--- termux/termux-base.sh
+++ termux/termux-base.sh
@@ -2,9 +2,20 @@ termux-setup-storage
 
 pkg up
 
-pkg in game-repo science-repo unstable-repo x11-repo root-repo termux-apt-repo
+pkg in -y game-repo science-repo unstable-repo x11-repo root-repo termux-apt-repo
 
-pkg in git wget curl openssh nano neofetch htop man 
+pkg in -y git wget curl openssh nano neofetch htop man 
+
+# Setting up ssh keys if they aren't already
+if [ ! -d ~/.ssh ]; then
+    ssh-keygen -N "" -f ~/.ssh/id_rsa
+else
+    echo "SSH keys already exist"
+fi
+
+# youtube-dl
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
+chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
 
 # oh-my-zsh
 sh ../oh-my-zsh-full.sh
