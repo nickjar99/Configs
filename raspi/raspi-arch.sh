ea80f805edaba5c9d972c15386a72a81e9bd00b9
Nick <nickjar99@gmail.com>
2020-03-10 20:39:45 -0600

    Hopefully this works

--- raspi/raspi-arch.sh
+++ raspi/raspi-arch.sh
@@ -7,4 +7,19 @@ dhcpcd
 
 pacman -Syuu
 
-pacman -S 
+pacman -S - < archRpiPackages.txt
+
+userdel -r alarm
+echo "%wheel ALL=(ALL) NOPASSWD: ALL"
+useradd -m -G wheel nick
+
+echo "Set root password:"
+passwd
+echo "Setting password for nick:"
+passwd nick
+
+
+echo "arch-pi" > /etc/hostname
+sed 's/# en_US.UTF-8 UTF-8/en_US.UTF-8'
+echo "LANG=en.US.UTF-8" > /etc/locale.conf
+locale-gen
