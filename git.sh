298af0f7a109372ab02928c4852db8dd4b396577
njarmusz <njarmusz@mymail.mines.edu>
2020-04-02 20:06:01 -0900

    more git configs for transferring larger files

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
 git config credential.helper store
 git config --global user.email "njarmusz@mymail.mines.edu"
 git config --global user.name "njarmusz"
-
+git config --global http.postBuffer 2097152000
+git config --global https.postBuffer 2097152000
 
