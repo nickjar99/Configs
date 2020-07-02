0c8a61ca130e4aa29a5782236d9160841bb2f667
Nick Jarmusz <nickjar99@gmail.com>
2020-07-02 04:23:52 -0500

    yes

--- wsl/wsl-kali.sh
+++ wsl/wsl-kali.sh
@@ -1,5 +1,4 @@
 sudo apt update
-sudo apt upgrade
-sudo apt install wget curl git vim nano neofetch htop ncdu unzip rar zsh openssh-server powerline
-
-sudo apt install speedtest-cli hdparm python3-pip glances ffmpeg
+sudo apt upgrade -y
+sudo apt install -y wget curl git vim nano neofetch htop ncdu unzip rar zsh openssh-server powerline \\
+speedtest-cli hdparm python3-pip glances ffmpeg
