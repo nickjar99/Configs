4483dd01f87e72b1871700e66605b01532a696e9
Nick Jarmusz <nickjar99@gmail.com>
2020-07-02 04:32:31 -0500

    git

--- wsl/wsl-kali.sh
+++ wsl/wsl-kali.sh
@@ -3,4 +3,5 @@ sudo apt upgrade -y
 sudo apt install -y wget curl git vim nano neofetch htop ncdu unzip rar zsh openssh-server powerline \\
 speedtest-cli hdparm python3-pip glances ffmpeg
 
+sh ../git.sh
 sh ../zsh-full.sh
