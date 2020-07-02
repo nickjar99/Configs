bbc7acb730f1ea78685b7567f91a7c22e0f2ec61
Nick Jarmusz <nickjar99@gmail.com>
2020-07-02 04:08:20 -0500

    aa

--- wsl/wsl-kali.sh
+++ wsl/wsl-kali.sh
@@ -1,8 +1,5 @@
-
-
 sudo apt update
 sudo apt upgrade
-sudo apt install openssh-server wget curl git vim nano neofetch htop ncdu unzip rar zsh powerline
+sudo apt install wget curl git vim nano neofetch htop ncdu unzip rar zsh openssh-server powerline
 
-
-sudo apt install speedtest-cli hdparm python3-pip glances
+sudo apt install speedtest-cli hdparm python3-pip glances ffmpeg
