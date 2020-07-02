7cf49400abab96aad18f9d324f801f56fc689971
Nick Jarmusz <nickjar99@gmail.com>
2020-07-02 04:39:54 -0500

    path

--- wsl/wsl-kali.sh
+++ wsl/wsl-kali.sh
@@ -1,7 +1,11 @@
+# directory stuff
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+chdir $DIR
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y wget curl git vim nano neofetch htop ncdu unzip rar zsh openssh-server powerline \\
-speedtest-cli hdparm python3-pip glances ffmpeg
+ hdparm python3-pip glances ffmpeg
 
 sh ../git.sh
 sh ../zsh-full.sh
