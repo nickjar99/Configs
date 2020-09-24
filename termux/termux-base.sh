c2882738a99fa17a6a6cd441aa3de00bcf57160a
njarmusz <njarmusz@mymail.mines.edu>
2020-09-24 00:23:13 -0600

    less janky termux setup

--- termux/termux-base.sh
+++ termux/termux-base.sh
@@ -6,7 +6,7 @@ pkg up -y
 
 pkg in -y game-repo science-repo unstable-repo x11-repo root-repo
 
-pkg in -y git wget curl openssh python neofetch htop man termux-api zsh tigervnc nano vim zip ncdu ffmpeg tsu build-essential:w
+pkg in -y git wget curl openssh python neofetch htop man termux-api zsh tigervnc nano vim zip ncdu ffmpeg tsu build-essential bsdtar proot   
 
 
 git config credential.helper store                                                                                      git config --global user.email "njarmusz@mymail.mines.edu"                                                              git config --global user.name "njarmusz" 
@@ -19,9 +19,8 @@ else
 fi
 sshd
 
-if [! -f ~/.termux/boot/start-sshd]; then
-    echo "#!/data/data/com.termux/files/usr/bin/sh\n    termux-wake-lock\n    sshd"
-fi
+echo "#!/data/data/com.termux/files/usr/bin/sh\n    termux-wake-lock\n    sshd" > ~/.termux/boot/start-sshd  
+
 
 # youtube-dl
 curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
@@ -39,16 +38,19 @@ echo "alias ytdl=youtube-dl\n" >> ~/.zshrc
 echo "alias gcl=\"git clone\"\n" >> ~/.zshrc
 echo "alias nf=\"neofetch\"\n" >> ~/.zshrc
 
-echo "alias home=\"cd ~/storage/shared\""
+echo "alias home=\"cd ~/storage/shared\"" >> ~/.zshrc
 chsh -s zsh
 
 rm install.sh
 
 # vnc (partial)
-if  [ ! -f ~/.vnc/passwd ]; then vncpasswd; fi
+# if  [ ! -f ~/.vnc/passwd ]; then vncpasswd; fi
 
 # its-pointless repo
 curl -LO https://its-pointless.github.io/setup-pointless-repo.sh
 bash setup-pointless-repo.sh
 
+
 echo "Finished script!"
+
+
