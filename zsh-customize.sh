dc4772f8d56645273b1dc063fc3f03943f477652
njarmusz <njarmusz@mymail.mines.edu>
2020-10-27 01:57:38 -0600

    youtube-dl

--- zsh-customize.sh
+++ zsh-customize.sh
@@ -8,3 +8,8 @@ sed "219d; 91,94d; 90s/{/{}/" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
 echo "alias ytdl=youtube-dl\n" >> ~/.zshrc
 echo "alias gcl=\"git clone\"\n" >> ~/.zshrc
 echo "alias nf=\"neofetch\"\n" >> ~/.zshrc
+
+
+echo "function y () { youtube-dl -o "%(title)s.%(ext)s" $@ }" >> ~/.zshrc
+
+
