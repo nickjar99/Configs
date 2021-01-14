26d9f57e9f9fa344f7d5ff47f09179569d8bfd79
njarmusz <njarmusz@mymail.mines.edu>
2021-01-14 02:29:37 +0000

    oh my zsh streamlining

---
+++ zsh-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Reinstalling oh-my-zsh
+rm -rf ~/.oh-my-zsh
+curl -o install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+rm install.sh
+
+sh zsh-customize.sh
