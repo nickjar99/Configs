035801a65d15be3161d2f39a3b0071604ceaf03f
njarmusz <njarmusz@mymail.mines.edu>
2021-01-13 17:52:18 -0700

    better oh my zsh

--- zsh-customize.sh
+++ zsh-customize.sh
@@ -2,8 +2,10 @@
 
 echo "Customizing oh-my-zsh install..."
 
-sed "11s/robbyrussell/agnoster/" -i ~/.zshrc
-sed "219d; 91,94d; 90s/{/{}/" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
+# takes away left side of status bar
+#sed "11s/robbyrussell/agnoster/" -i ~/.zshrc
+#sed "219d; 91,94d; 90s/{/{}/" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
+
 # aliases
 echo "alias ytdl=youtube-dl\n" >> ~/.zshrc
 echo "alias gcl=\"git clone\"\n" >> ~/.zshrc
