38c690147c7ca4fea5948da90dd7bab10fb42a6a
njarmusz <njarmusz@mymail.mines.edu>
2021-02-24 12:36:47 -0700

    termux

--- termux/termux-full.sh
+++ termux/termux-full.sh
@@ -3,7 +3,7 @@
 sh termux/termux-base.sh
 
 # More packages
-pkg in -y golang rust unzip zip dos2unix unrar tty-clock imagemagick tigervnc shared-mime-info screenfetch screen ruby ripgrep readline-static pkg-config p7zip ffmpeg opusfile-static opus-tools ninja ncdu imagemagick help2man graphicsmagick golang-doc gdb fdupes feh fakeroot emacs dialog cowsay clang cmake ccache bsdtar bash-completion autoconf automake termux-services termux-apt-repo ccache automake autoconf autossh
+pkg in -y golang rust unzip zip dos2unix unrar tty-clock imagemagick tigervnc shared-mime-info screenfetch screen ruby ripgrep readline-static pkg-config p7zip ffmpeg opusfile-static opus-tools ninja ncdu imagemagick help2man graphicsmagick golang-doc gdb fdupes feh fakeroot emacs dialog cowsay clang cmake ccache bsdtar bash-completion autoconf automake termux-services termux-apt-repo ccache automake autoconf autossh mediainfo
 
 
 # Python stuff
@@ -11,7 +11,7 @@ pip install --upgrade pip
 pip install --user pylint
 
 # other stuff
-pkg in -y  hwinfo hwprobe openvpn
+pkg in -y  hwinfo openvpn
 
 # epic hax
 pkg in -y aircrack-ng wavemon iw ipset ipset-static
