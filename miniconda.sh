04cde5d99c453e69558a09ea350c18dc0dcedeba
nickjar99 <nickjar99@gmail.com>
2021-03-02 23:37:55 -0700

    miniconda+cuda?

---
+++ miniconda.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+curl -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
---
+++ wsl/cuda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y cuda-toolkit-11-0
+
+echo "Sample location: /usr/local/cuda/samples"
