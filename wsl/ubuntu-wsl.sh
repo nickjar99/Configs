5ee7354d60be276828d4edd7651ccef48fae22cb
njarmusz <njarmusz@mymail.mines.edu>
2021-03-02 23:13:49 -0700

    zsh, wsl

--- wsl/ubuntu-wsl.sh
+++ wsl/ubuntu-wsl.sh
@@ -6,6 +6,7 @@ sudo apt install -y wget curl git vim nano neofetch \
  htop ncdu unzip rar zsh openssh-server powerline \
  hdparm speedtest-cli python3 python3-pip glances ffmpeg
 
-sh git.sh
+sh ../git.sh
 # sh zsh-full.sh
-sh oh-my-zsh-custom.sh
+# sh oh-my-zsh-custom.sh
+sh ../zsh-setup.sh
--- zsh-setup.sh
+++ zsh-setup.sh
@@ -3,6 +3,7 @@
 echo Reinstalling oh-my-zsh
 rm -rf ~/.oh-my-zsh
 curl -o install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh install.sh
 rm install.sh
 
 sh zsh-customize.sh
