61dd0ffa871340a596c5a4f60059ba20d5bba711
nickjar99 <nickjar99@gmail.com>
2021-03-02 23:44:04 -0700

    ubu

--- wsl/ubuntu-wsl.sh
+++ wsl/ubuntu-wsl.sh
@@ -4,7 +4,14 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y wget curl git vim nano neofetch \
  htop ncdu unzip rar zsh openssh-server powerline \
- hdparm speedtest-cli python3 python3-pip glances ffmpeg
+ hdparm speedtest-cli python3 python3-pip glances ffmpeg mediainfo zip rclone rsync
+
+# Setting up ssh keys if they aren't already
+if [ ! -d ~/.ssh ]; then
+    ssh-keygen -N "" -f ~/.ssh/id_rsa
+else
+    echo "SSH keys already exist"
+fi
 
 bash ../git.sh
 # sh zsh-full.sh
