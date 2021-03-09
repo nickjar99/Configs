418cbc124f678ee78f27ee06806d8093e79b2dd3
nickjar99 <nickjar99@gmail.com>
2021-03-09 06:10:26 -0700

    nuser

--- termux/proot-distro-arch.sh
+++ termux/proot-distro-arch.sh
@@ -5,7 +5,7 @@ pacman -Syuu --noconfirm git wget curl zsh sudo vim
 useradd -m -G wheel user
 
 sed -i "s/#* *%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
-echo nuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 echo "Enter Root Password:"
 passwd
