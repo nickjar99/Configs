94424b04a07014a9760e3c301ba36de89416c1c0
nickjar99 <nickjar99@gmail.com>
2022-10-11 23:13:02 -0600

    termux

--- git.sh
+++ git.sh
@@ -6,4 +6,4 @@ git config --global user.email "nickjar99@gmail.com"
 git config --global user.name "nickjar99"
 git config --global http.postBuffer 2097152000
 git config --global https.postBuffer 2097152000
-
+git config --global --add safe.directory '*'
--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-bash git.sh
-bash zsh-setup.sh "$@"
+#bash git.sh
+#bash zsh-setup.sh "$@"
 
 # checking if on termux
 name=$(uname -r)
-if [[ *"$name"* == "android" ]]; then
+if [[ "$name" == *"android"* ]]; then
 	echo "Using termux"
-done
+fi
