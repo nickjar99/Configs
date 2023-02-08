515f06b13c9cde5cb1c3de2584488ba6365ca742
nickjar99 <nickjar99@gmail.com>
2023-02-08 08:31:43 -0700

    zsh aliases for easy navigation

---
+++ termux-config.sh
@@ -0,0 +1,4 @@
+
+
+echo 'alias h="cd ~/storage/shared"' >> ~/.zshrc
+echo 'alias school="cd ~/storage/shared/School"' >> ~/.zshrc
---
+++ wsl-config.sh
@@ -0,0 +1,4 @@
+
+
+echo 'alias h="cd /mnt/c/Users/nickj"' >> ~/.zshrc
+echo 'alias school="cd /mnt/d/School"' >> ~/.zshrc
