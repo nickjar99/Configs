232d6c5bf1c9ac77aa0d5e31ffd7977f7c489d99
nickjar99 <nickjar99@gmail.com>
2023-03-08 12:59:09 -0700

    fixed zsh aliases

--- zsh-customize.sh
+++ zsh-customize.sh
@@ -2,23 +2,13 @@
 
 echo "Customizing oh-my-zsh install..."
 
-# takes away left side of status bar
 
-echo "$1"
-if [[ "$1" == "agnoster" ]]; then
-    echo "Theme: Agnoster"
-    sed "11s/robbyrussell/agnoster/" -i ~/.zshrc
-    # sed "219d; 91,94d; 90s/{/{}/" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
-    sed "s/  prompt_git/#  prompt_git/g" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
-else
-    echo "Theme: Default"
-    sed "11s/agnoster/robbyrussell/" -i ~/.zshrc
-fi
+sed "11s/robbyrussell/agnoster/" -i ~/.zshrc
+sed "s/  prompt_git/#  prompt_git/g" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 # aliases
-echo "alias ytdl=youtube-dl\n" >> ~/.zshrc
-echo "alias gcl=\"git clone\"\n" >> ~/.zshrc
-echo "alias nf=\"neofetch\"\n" >> ~/.zshrc
+echo "alias gcl=\"git clone\"" >> ~/.zshrc
+echo "alias nf=\"neofetch\"" >> ~/.zshrc
 
 echo "function gcl () { git clone $@ }" >> ~/.zshrc
 echo "function gpl () { git pull }" >> ~/.zshrc
