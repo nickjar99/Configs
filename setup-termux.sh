8d98b2ed5d592bccacf9f21aa1d26690dbbbf47d
nickjar99 <nickjar99@gmail.com>
2023-03-09 14:48:26 -0700

    specific config files

--- setup-termux.sh
+++ setup-termux.sh
@@ -1,4 +1,3 @@
 
-
 echo 'alias h="cd ~/storage/shared"' >> ~/.zshrc
 echo 'alias school="cd ~/storage/shared/School"' >> ~/.zshrc
---
+++ setup-wsl.sh
@@ -0,0 +1,5 @@
+
+echo 'alias h="cd /mnt/c/Users/nickj"' >> ~/.zshrc
+echo 'alias d="cd /mnt/d"' >> ~/.zshrc
+
+echo 'alias school="cd /mnt/d//School"' >> ~/.zshrc
