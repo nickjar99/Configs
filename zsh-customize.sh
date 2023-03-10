816f403017a100826a7d0025311b97fe57108550
nickjar99 <nickjar99@gmail.com>
2023-03-10 16:31:24 -0700

    ye

--- zsh-customize.sh
+++ zsh-customize.sh
@@ -16,7 +16,7 @@ sed "s/  prompt_git/#  prompt_git/g" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
 sed "91s/ \".*||//" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 # removing X after failed command
-sed "235s/.*//" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
+sed "235s/.*/#/" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 # aliases
 echo "alias nf='neofetch'" >> ~/.zshrc
