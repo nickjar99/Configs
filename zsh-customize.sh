ec46d7c421646c53ec562c49f7211e259e90b672
nickjar99 <nickjar99@gmail.com>
2023-03-10 17:01:07 -0700

    looks good now

--- zsh-customize.sh
+++ zsh-customize.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 echo "Customizing oh-my-zsh install..."
+agnoster="$HOME/.oh-my-zsh/themes/agnoster.zsh-theme"
 
 echo "$1"
 # Prompt color
 if [ ! -z $1 ]
 then
-	sed "218s/blue/$1/" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
+	sed "218s/blue/$1/" -i $agnoster
 fi
 
 sed "11s/robbyrussell/agnoster/" -i ~/.zshrc
-sed "s/  prompt_git/#  prompt_git/g" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
+sed "s/  prompt_git/#  prompt_git/g" -i $agnoster 
 
-# removing unnecessary username
-sed "91s/ \".*||//" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
+# removing unnecessary username but keeping the tail
+sed "91s/ \".*||//" -i $agnoster 
+sed "93s/fi/else; prompt_segment black default; fi/" -i $agnoster 
 
 # removing X after failed command
-sed "235s/.*/#/" -i ~/.oh-my-zsh/themes/agnoster.zsh-theme
+sed "235s/.*/#/" -i $agnoster
+
 
 # aliases
 echo "alias nf='neofetch'" >> ~/.zshrc
