6589fda22e87d32e1ec95c5dd041c9ca36d73d3c
nickjar99 <nickjar99@gmail.com>
2023-03-27 16:26:24 -0600

    new ip & hostname showing tool

--- setup-termux.sh
+++ setup-termux.sh
@@ -9,6 +9,5 @@ echo "alias pi='pkg install'" >> ~/.zshrc
 echo "alias ps='pkg search'" >> ~/.zshrc
 
 # gets ip address
-echo "alias ipa=\"sudo ip a | grep UP -A 2 | grep inet | grep wlan | sed \\\"s/\ .*inet \(.*\)\/.*/\1/\\\"\"" >> ~/.zshrc
-echo "alias ipa2=\"echo -n "$(whoami)@" && sudo ip a | grep UP -A 2 | grep inet | grep wlan | sed \\\"s/\ .*inet \(.*\)\/.*/\1/\\\"\"" >> ~/.zshrc
+echo "alias ipa=\"echo -n "$(whoami)@" && sudo ip a | grep UP -A 2 | grep inet | grep wlan | sed \\\"s/\ .*inet \(.*\)\/.*/\1/\\\"\"" >> ~/.zshrc
 
